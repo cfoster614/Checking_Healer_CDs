fad532abf7c964fb35005559f12779486fbb3637
Caity <cjaynefoster@gmail.com>
2023-06-26 13:56:36 -0400

    Added bosses and spells logic from server upon loading homepage.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,17 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
+
 from app.main import bp
+
+from app.buckets.bosses import get_boss_list
+from app.buckets.spells import get_spell_info
 
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    return redirect('/healers_home')
 
+@bp.route('/healers_home')
+def homepage():
+    bosses = get_boss_list()
+    spells = get_spell_info()
+    return render_template('index.html', bosses=bosses, spells=spells)
