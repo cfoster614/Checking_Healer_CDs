096a0e4fa80a1d8b0f27a59cec5b0e78f5fe071a
Caity <cjaynefoster@gmail.com>
2023-06-26 13:55:43 -0400

    Added Query class. Modified Boss and Spell class to also require the icon url.

--- app/models/warcraftlogs.py
+++ app/models/warcraftlogs.py
@@ -1,17 +1,40 @@
+from app.API_queries import url, headers
+import requests
+
+
 class Boss:
     """Bosses for Aberrus."""
     
-    def __init__(self, id, name):
+    def __init__(self, id, name, icon):
         self.id = id
         self.name = name
+        self.icon = icon
     def __repr__(self):
-        return f"<id={self.id}, name={self.name}>"
+        return f"<id={self.id}, name={self.name}, icon={self.icon}>"
+     
         
 class Spell:
     """Major healing cooldown spells."""
     
-    def __init__(self, id, name):
+    def __init__(self, id, name, icon):
         self.id = id
         self.name = name
+        self.icon = icon
+        
     def __repr__(self):
-        return f"<id={self.id}, name={self.name}>"
+        return f"<id={self.id}, name={self.name}, icon={self.icon}>"
+
+    
+class Query:
+    """For an easier time running queries to the API."""
+    def __init__(self, query):
+        self.query = query
+        
+    def run_query(self):
+        data = {
+            'query' : self.query
+        }
+        response = requests.get(url, headers=headers, json=data)
+        data = response.json()
+        
+        return data
