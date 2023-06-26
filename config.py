3197e26b602830072ec253e1d8c67cd7aa8d481e
Caity <cjaynefoster@gmail.com>
2023-06-26 13:58:08 -0400

    Updated.

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql:///healer_assignments')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql:///healer_assignments')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
