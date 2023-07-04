f5002fac129269c036f77fad287de2de27f194b7
Caity <cjaynefoster@gmail.com>
2023-07-04 11:31:19 -0400

    Cleaned up code. Since over 200 spells are in the database now, made it so createNewSpellsClasses can filter out repeats.

--- app/static/apiQuerying.js
+++ app/static/apiQuerying.js
@@ -3,45 +3,36 @@ and am just using pictures of the bosses */
 
 let cachedSpells = [];
 
-
 async function getSpellInfo() {
     //Query the api for spell info.
     if (cachedSpells.length === 0) {
         const resp = await axios.get('http://127.0.0.1:5000/api.healer_cds.com/spells');
+
+        // filteredSpells = filterObj(resp.data.spells)
         cachedSpells = createNewSpellsClasses(resp.data.spells)
+
     }
     return cachedSpells
 }
 
 
-function createNewSpellsClasses(arr) {
-    //Turn the array of objects of spells into their own individual classes.
-    //Makes it easier to access them later on.
-    let id;
-    let name;
-    let icon;
+    function createNewSpellsClasses(arr) {
+        //Turn the array of objects of spells into their own individual classes.
+        //Makes it easier to access them later on.
+        //Create a new list that has all of the new Spell classes.
+        let filteredSpells = [];
+        for (let i = 0; i < arr.length; i++) {
+            const name = (arr[i]['name']);
+            
+            if(!filteredSpells.includes(name)){
+                const newSpell = new Spell((arr[i]['id']), name, (arr[i]['icon']) );
+                cachedSpells.push(newSpell);
+                filteredSpells.push(name);
 
-    //Create a new list that has all of the new Spell classes.
-
-    for (let i = 0; i < arr.length; i++) {
-        for (let key in arr[i]) {
-            if (key.includes('id')) {
-                id = (arr[i][key])
-            }
-            if (key.includes('name')) {
-                name = (arr[i][key])
-            }
-            if (key.includes('icon')) {
-                icon = (arr[i][key])
             }
         }
-        const newSpell = new Spell(id, name, icon)
-        cachedSpells.push(newSpell)
+        return cachedSpells
     }
-    return cachedSpells
-}
 
 
-getSpellInfo()
-
-
+    getSpellInfo()
