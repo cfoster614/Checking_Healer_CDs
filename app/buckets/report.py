463dbdc7a3ce990d0f4372e1e28f365e96e1e15a
Caity <cjaynefoster@gmail.com>
2023-07-11 13:56:28 -0400

    correct_spell function query now uses spell name, eliminating the need for the extra inputs. Deleted old function.

--- app/buckets/report.py
+++ app/buckets/report.py
@@ -1,17 +1,18 @@
-from app.models.warcraftlogs import run_query, Player_Event, readable_time
+from app.models.warcraftlogs import run_query, readable_time
 from app.models.logs_database import Spell
 
-
 def url_to_log_code(url):
-    """
-    Split the url the user submitted to just the code for API query.
-    Example: 
-    """
+    """Split the url the user submitted to just the code for API query."""
     code= url.split("/")[-1]
   
     return code
 
 def report_data(code, boss):
+    """
+    All the info we need for report html page.
+    Just need the report code and the encounterID.
+    Gives list of all wipes/kills from that boss.
+    """
     query = f'''
         query reportData {{
             reportData {{report(code: "{code}"){{
@@ -33,11 +34,9 @@ def report_data(code, boss):
     try:
         encounters= results['data']['reportData']['report']['fights']
         
-        print(encounters)
-        
-        
     except (KeyError) as e:
         print(f"Error accessing response data: {e}")
+        
     return encounters
 
 
@@ -71,7 +70,7 @@ def fight_data(code, fightID):
     timers = results['data']['reportData']['report']['fights']
     
     for time in timers:
-       
+               
         start_time = (time['startTime'])
         s_time = readable_time(start_time)
       
@@ -84,50 +83,19 @@ def fight_data(code, fightID):
                      'd_time' : d_time}
        
         return time_dict
-        
-       
-# def correct_spell(spell_list, code, fightID, fight_info):
-   
-#     spells_list = [Spell.query.filter_by(spell_id = spell_id).all() for spell_id in spell_list]
     
-        
-#     spell_data = []
-#     for spell in spells_list: #Run a query for every spell with the correct name. 
-#         for s in spell:
-#             print(spell)
-#             query = f'''
-#                 query reportData {{ 
-#                     reportData {{report(code: "{code}") {{
-#                         graph(dataType: Casts, fightIDs: {fightID}, abilityID: {s.spell_id})
-#                     }}
-#                 }}
-#             }}
-#             '''
-#             results = run_query(query)  
-#             spell_info = results['data']['reportData']['report']['graph']['data']['series']
-
-
-
-#                 #Since cast timestamps are relative to startTimes, we need to subtract from the cast timestamp.
-#                 #Convert like usual after the subtraction for cast times.     
-#                 # casts = [readable_time(s['timestamp'] - start_time) for s in spell]
-#             spell_class= Player_Event(spell_info, fight_info)
-#             spell_data.append(spell_class)
-        
-    
-#     return spell_data
-            
             
 def correct_spell(spell_list, code, fightID, fight_info):
-    print(fight_info)
-   
-    spells_list = [Spell.query.filter_by(spell_id = spell_id).all() for spell_id in spell_list]
+    """
+    Query report using the spells given and the fightID. 
+    This gives us all the spell data/player data we need.
+    """
     
+    spells_list = [Spell.query.filter_by(name = spell_name).all() for spell_name in spell_list] #Take spell list and make new list with spell ids for query.
         
     spell_data = []
-    for spell in spells_list: #Run a query for every spell with the correct name. 
+    for spell in spells_list: #Run a query for every spell given. 
         for s in spell:
-          
             query = f'''
                 query reportData {{ 
                     reportData {{report(code: "{code}") {{
@@ -139,18 +107,11 @@ def correct_spell(spell_list, code, fightID, fight_info):
             results = run_query(query)  
             spell_info = results['data']['reportData']['report']['graph']['data']['series']
 
-
-
-                #Since cast timestamps are relative to startTimes, we need to subtract from the cast timestamp.
-                #Convert like usual after the subtraction for cast times.     
-                # casts = [readable_time(s['timestamp'] - start_time) for s in spell]
-            simplified_data = []
+            simplified_data = [] #Data comes out with more than we need. Makes it difficult to sort through.
 
             for player_data in spell_info:
                 
-               
                 name = player_data['name']
-                timestamps = []
                 abilities = []
 
                 for event in player_data['events']:
