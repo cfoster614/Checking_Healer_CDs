459b094bc411f5468bf59a837f80490f975b6b3f
Caity <cjaynefoster@gmail.com>
2023-07-11 16:16:17 -0400

    Deleted unnecessary for loop. Deleted missed hidden input. Fixed original spell time input to not change name.

--- app/static/app.js
+++ app/static/app.js
@@ -10,15 +10,11 @@ function addSpellHandler() {
     
     const newSpellInput = $('<input>', { type: 'text', class: 'spells', name: 'spells', placeholder: 'Revival' }); 
     const newTimerBtn = $('<button>', { type: "button", class: 'btn timer-btn', text: 'Add new time' });
-
     const ulDiv = $('<div>', { class: 'suggestions-box' }).append($('<ul>', { id: 'spell-suggestions', class: 'suggestions' }));
     const spellWrapDiv = $('<div>', { class: 'spell-wrapper flex' }).append(newSpellInput, newTimerBtn)
-
     const newDiv = $('<div>', { class: 'spell-input-box' }).append($('<div>', { class: 'form-header flex' }), spellWrapDiv, $('<div>', { class: 'timers-wrapper' }), ulDiv)
 
-
     $('#spell-form').append(newDiv);
-
 }
 
 function addTimerHandler(e) {
@@ -32,7 +28,6 @@ function addTimerHandler(e) {
     const spellNames = getSpellNames();
     const inputBox = $(e.target).closest('.spell-input-box');
     const container = inputBox.find('.timers-wrapper');
-    
     const value = $(e.target).siblings('.spells')
     let inputVal = value.val();
     
@@ -63,8 +58,8 @@ function addTimerHandler(e) {
 
         //Create new timer inputs.
         const newInput = $('<input>', { type: 'text', class: 'timers', name: `${inputVal}-timers`, placeholder: '00:10'});
-        $(e.target).closest('.spell-input-box').find('.timers-wrapper').append(newInput); 
-        $(e.target).closest('.spell-input-box').find('.timers').attr('name', `${inputVal}-timers`) /**Change the previous inputs to the new spell for the 'name'.
+       inputBox.find('.timers-wrapper').append(newInput); 
+        inputBox.find('.timers').attr('name', `${inputVal}-timers`) /**Change the previous inputs to the new spell for the 'name'.
                                                                                                     *This way we can group the timers by spell in the backend.
                                                                                                     */
         
@@ -104,60 +99,48 @@ function searchHandler(e) {
 function showSuggestions(obj, target) {
     /* Show suggestions of spells based on letters input. */
 
-
     $('.suggestions').empty(); //Start with a fresh slate for each keypress
 
     let spellNames = filterClass(obj);
     let filteredSpells = search(spellNames, target);
-
 
     for (spell of cachedSpells) {
         if (filteredSpells.includes(spell.name)) {
             $('.suggestions').append(`<li data-index-number=${spell.id}><img src=${spell.icon}>${spell.name}</li>`);
         }
     }
-
 }
-
-
-
-
 
 function useSuggestion(e) {
     /** Change the input val to the suggested spell text. */
-
-    let suggestionText = e.target.innerText;
-    let suggestionValue = $(e.target).attr('data-index-number');
-    const inputElement = $(e.target).closest('.spell-input-box').find('.spells');
-    const hiddenInput = $(e.target).closest('.spell-input-box').find('.hidden-ids');
-    const headerText = $(e.target).closest('.form-header').find('h4').text();
-    const header = $(e.target).closest('.spell-input-box').find('.form-header');
-
-
-
-
-    if ($(e.target).is('img')) {
-        suggestionText = $(e.target).closest('li').text(); //We want the user to be able to click the img or the text.
-        suggestionValue = $(e.target).closest('li').attr('data-index-number');
-        console.log(suggestionValue)
+    const target = e.target;
+    console.log(target)
+    let suggestionText = $(target).text();
+    const spellInputBox = $(target).closest('.spell-input-box');
+    const inputElement = spellInputBox.find('.spells');
+    const timers = spellInputBox.find('.timers');
+    const header = spellInputBox.find('.form-header');
+    const headerText = $(target).closest('.form-header').find('h4').text();
+    const spellNames = getSpellNames();
+    if ($(target).is('img')) { //We want the user to be able to click the img or the text.
+        suggestionText = $(target).closest('li').text(); 
     }
 
     inputElement.val(suggestionText);
-    hiddenInput.val(suggestionValue);
+    timers.attr('name', `${suggestionText}-timers`); //Change timer input to match spell name put in.
 
-
-    for (spell of cachedSpells) {
-        if (spell.name === suggestionText) {
-            if (header.children().length === 0 || headerText !== suggestionText) {
-                header.empty();
-                header.append($('<img>', { src: spell.icon }));
-                header.append($('<h4>', { text: spell.name }));
-                break;
-
-            }
+     
+    if (suggestionText.indexOf(spellNames)) { //For the header text/image.
+        if (header.children().length === 0 || headerText !== suggestionText) {
+            const img = $(target).find('img');
+            header.empty();
+            header.append($('<img>', { src: `${img.attr('src')}`}));
+            header.append($('<h4>', { text: $(target).text() }));
+            
         }
     }
-    $('#spell-suggestions').empty(); //Clear suggestions after clicking suggesion.
+    
+    $('#spell-suggestions').empty(); //Clear suggestions after clicking suggestion.
 }
 
 
