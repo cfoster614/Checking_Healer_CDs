a15cd2ba98626e7f5d7077e0e6d894ba9c6dad40
Caity <cjaynefoster@gmail.com>
2023-07-27 14:03:30 -0400

    Added function for organizing the spell and player data from the frontend.

--- app/models/warcraftlogs.py
+++ app/models/warcraftlogs.py
@@ -1,5 +1,8 @@
-from app.API_queries import url, headers
+from flask import request
+from app.models.logs_database import Spell
 import requests
+import os
+token = os.environ.get('API_TOKEN')
 
 def serialized(self):
     return {
@@ -19,6 +22,12 @@ def serialized_spell(self):
 
 def run_query(query):
     """Helper function for queries."""
+    headers = {
+    'Authorization' : f'Bearer {token}'
+    }
+    
+    url = "https://www.warcraftlogs.com/api/v2/client"
+    
     data = {
         'query' : query
     }
@@ -42,3 +51,35 @@ def readable_time(time):
     formatted_time = f"{minutes:02d}:{rem_sec:02d}"
     
     return formatted_time
+
+def filter_spells(players):
+    spell_list = []
+    for spell in players:
+        print(spell)
+        spell_list.append(spell['name'])
+    return spell_list
+
+def get_spell_times_from_form(players):
+    spell_info_list = []
+    seen_combination = set()
+    
+    for player in players:
+        spells = request.form.getlist(f'spells {player}')
+        for spell in spells:
+            combination = (player, spell)
+         
+            if combination not in seen_combination:
+                times = request.form.getlist(f'{spell} {player} timers')
+                seen_combination.add(combination)
+
+                spell_info = Spell.query.filter_by(name=spell).first()
+                spell_info_dict = {
+                    'name': spell,
+                    'times': times,
+                    'icon': f'{spell_info.icon}',
+                    'player_name': player
+                }
+                spell_info_list.append(spell_info_dict)
+    
+    return spell_info_list
+
